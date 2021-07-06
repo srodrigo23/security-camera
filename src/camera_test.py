3617647496b00032749e4736ab5a4c9101cdb2f3
rodres101 <rodrigosergio93@gmail.com>
2021-07-05 21:34:32 -0400

    adding some fixes

--- src/camera_test.py
+++ src/camera_test.py
@@ -10,9 +10,9 @@ time.sleep(1.0) # to prepare camera
 #TODO: resize image
 while True:
     frame = stream.read()    
-    # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    # frame = np.dstack([frame, frame, frame])
-    frame = resize(frame, width=450) #resize image
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    frame = np.dstack([frame, frame, frame])
+    frame = resize(frame, width=320) #resize image
     cv2.imshow("Frame", frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
--- src/stream.py
+++ src/stream.py
@@ -13,8 +13,14 @@ def stream(tcp):
     node_cam = CameraStream(usePiCamera=False)
     node_cam.start()
     time.sleep(2.0) # allow camera sensor to warm up
+    i = 0
     while True:
         image = node_cam.read()
+        image = resize(image, width=320) #resize image
+        image = np.dstack([image, image, image])
         sender.send_image(node_name, image)
-        
+        print(f"Frame sent : {i}")
+        i=i+1
+        if keyboard.is_pressed('q'):
+            break
 stream(tcp)
